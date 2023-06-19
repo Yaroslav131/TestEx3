d5f622d255df1983aa54635755c2f32ec6e3c091
Yaroslav131 <berserk12186@gmail.com>
2023-06-19 22:26:28 +0300

    add interface for geo object

---
+++ src/interfaces/IGeoObject.ts
@@ -0,0 +1,12 @@
+export default interface IGeoObject {
+    id: number,
+    lat: number,
+    lon: number,
+    name: string,
+    amenity: string | null,
+    tourism: string | null,
+    website: string | null,
+    phone: string | null,
+    adress: string | null,
+    description: string | null,
+}
