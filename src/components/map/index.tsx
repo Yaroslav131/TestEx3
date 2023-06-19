b8d5cf78302630d51f106197fa576301a2afcc37
Yaroslav131 <berserk12186@gmail.com>
2023-06-19 22:22:10 +0300

    moved the variables to a higher level

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,34 +1,24 @@
 import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
-import { useEffect, useState } from 'react';
 
-import { getUserGeolocation } from '../../api/browserApi';
 import userMark from "../../assets/imgs/userMark.svg"
 
 import './styles.css'
 
-const MapComponent = () => {
- const [userCoords, setUserCoords] = useState<[number, number] >([55.751574, 37.573856]);
- 
-  useEffect(() => {
-    const unsubscribe = getUserGeolocation((latitude, longitude) => {
-      setUserCoords([latitude, longitude]);
-    });
+interface Iprops {
+  userCoords:[number,number]
+}
 
-    return () => {
-      unsubscribe();
-    };
-  }, []);
-
+const MapComponent = (props:Iprops) => {
   return (
     <YMaps query={{
       lang: "en_RU",
       apikey: "18f172d7-21c0-4d35-bac0-a89f15490ad1"
     }}>
       <div className="map-container">
-        <Map defaultState={{ center: userCoords, zoom: 15 }} width="100%" height="100%">
-          {userCoords && (
+        <Map defaultState={{ center: props.userCoords, zoom: 15 }} width="100%" height="100%">
+          {props.userCoords && (
             <Placemark
-              geometry={userCoords}
+              geometry={props.userCoords}
               options={{
                 iconLayout: 'default#image',
                 iconImageHref: userMark,
