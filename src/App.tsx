ef3afa47c4bcb16bb833271248b0ae86c8ce2392
Yaroslav131 <berserk12186@gmail.com>
2023-06-22 00:45:12 +0300

    fix spaces

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import {Provider} from "react-redux"
+import { Provider } from "react-redux"
 
-import './globalStyles.css'
 import store from "../src/store/store"
 import MainPage from './pages/mainPage'
+
+import './globalStyles.css'
 
 function App() {
   return (
     <Provider store={store}>
-   <MainPage/>
-   </Provider>
-
+      <MainPage />
+    </Provider>
   )
 }
 
