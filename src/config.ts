e9de945c238c5a419a609fba2cdcd73000526f3b
Yaroslav131 <berserk12186@gmail.com>
2023-06-21 12:59:59 +0300

    divided const

--- src/config.ts
+++ src/config.ts
@@ -15,31 +15,56 @@ import bicycleIcon from "./assets/imgs/attractionsIcons/bicycleIcon.svg";
 import bankIcon from "./assets/imgs/attractionsIcons/bankIcon.svg";
 import architectureIcon from "./assets/imgs/attractionsIcons/architectureIcon.svg";
 import eighteenPlusIcon from "./assets/imgs/attractionsIcons/18+Icon.svg";
+import other from "./assets/imgs/attractionsIcons/otherIcon.svg";
 
 interface ISeachOption {
     tag: string
-    optionIcon: string,
     description: string
 }
 
-export const attractionsOptions: ISeachOption[] = [
-    { optionIcon: natureIcon, tag: `"leisure"="park"`, description: "Природа, парки" },
-    { optionIcon: variantIcon, tag: `"amenity"="place_of_worship"`, description: "Религия" },
-    { optionIcon: sportIcon, tag: "sport", description: "Спортивные центры" },
-    { optionIcon: restaurantIcon, tag: "amenity=restaurant", description: "Рестораны" },
-    { optionIcon: marketIcon, tag: "shop=supermarket", description: "Магазины" },
-    { optionIcon: industryIcon, tag: "industrial", description: "Промышленность" },
-    { optionIcon: hostalIcon, tag: "tourism=hotel", description: "Отель" },
-    { optionIcon: historyIcon, tag: "historic=monument", description: "История" },
-    { optionIcon: gasStationIcon, tag: "amenity=fuel", description: "Заправка" },
-    { optionIcon: entertainmentIcon, tag: "leisure", description: "Развлечения" },
-    { optionIcon: cultureIcon, tag: "amenity=arts_centre", description: "Культура" },
-    { optionIcon: coffeeIcon, tag: "amenity=cafe", description: "Кофе" },
-    { optionIcon: carIcon, tag: "amenity=car_rental", description: "Каршеринг" },
-    { optionIcon: bicycleIcon, tag: 'amenity=bicycle_rental', description: 'Прокат велосипедов' },
-    { optionIcon: bankIcon, tag: "amenity=bank", description: "Банки" },
-    { optionIcon: architectureIcon, tag: "historic=architectural", description: "Архитектура" },
-    { optionIcon: eighteenPlusIcon, tag: "stripclub", description: "18+" },
+export const seachOptions: ISeachOption[] = [
+    { tag: '"leisure"="park"', description: "Природа, парки" },
+    { tag: '"amenity"="place_of_worship"', description: "Религия" },
+    { tag: "sport", description: "Спортивные центры" },
+    { tag: "amenity=restaurant", description: "Рестораны" },
+    { tag: "shop=supermarket", description: "Магазины" },
+    { tag: "industrial", description: "Промышленность" },
+    { tag: "tourism=hotel", description: "Отель" },
+    { tag: "historic=monument", description: "История" },
+    { tag: "amenity=fuel", description: "Заправка" },
+    { tag: "leisure", description: "Развлечения" },
+    { tag: "amenity=arts_centre", description: "Культура" },
+    { tag: "amenity=cafe", description: "Кофе" },
+    { tag: "amenity=car_rental", description: "Каршеринг" },
+    { tag: 'amenity=bicycle_rental', description: 'Прокат велосипедов' },
+    { tag: "amenity=bank", description: "Банки" },
+    { tag: "historic=architectural", description: "Архитектура" },
+    { tag: "stripclub", description: "18+" },
+];
+interface IGeoIcon {
+    tag: string
+    optionIcon: string,
+}
+
+export const geoIcons: IGeoIcon[] = [
+    { optionIcon: natureIcon, tag: '"leisure"="park"' },
+    { optionIcon: variantIcon, tag: '"amenity"="place_of_worship"' },
+    { optionIcon: sportIcon, tag: "sport" },
+    { optionIcon: restaurantIcon, tag: "amenity=restaurant" },
+    { optionIcon: marketIcon, tag: "shop=supermarket" },
+    { optionIcon: industryIcon, tag: "industrial" },
+    { optionIcon: hostalIcon, tag: "tourism=hotel" },
+    { optionIcon: historyIcon, tag: "historic=monument" },
+    { optionIcon: gasStationIcon, tag: "amenity=fuel" },
+    { optionIcon: entertainmentIcon, tag: "leisure" },
+    { optionIcon: cultureIcon, tag: "amenity=arts_centre" },
+    { optionIcon: coffeeIcon, tag: "amenity=cafe" },
+    { optionIcon: carIcon, tag: "amenity=car_rental" },
+    { optionIcon: bicycleIcon, tag: 'amenity=bicycle_rental' },
+    { optionIcon: bankIcon, tag: "amenity=bank" },
+    { optionIcon: architectureIcon, tag: "historic=architectural" },
+    { optionIcon: eighteenPlusIcon, tag: "stripclub" },
+    { optionIcon: other, tag: "search" },
 ];
 
 export const attractionsTags = [
