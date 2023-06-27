8960535260ded3be1c37bb229266a1c069af6099
Yaroslav131 <berserk12186@gmail.com>
2023-06-27 22:42:27 +0300

    fix bug

---
+++ src/components/SaveMenu/ChosenObject/Skeleton/index.tsx
@@ -0,0 +1,25 @@
+import ContentLoader from 'react-content-loader';
+
+const ChosenObjectSkeleton = () => {
+  return (
+    <ContentLoader
+      speed={2}
+      width={400}
+      height={280}
+      viewBox="0 0 400 280"
+      backgroundColor="#f3f3f3"
+      foregroundColor="#ecebeb"
+    >
+      <rect x="10" y="10" rx="10" ry="10" width="120" height="120" /> {/* Заглушка для изображения */}
+      <rect x="140" y="20" rx="4" ry="4" width="200" height="12" /> {/* Заглушка для заголовка */}
+      <rect x="140" y="50" rx="3" ry="3" width="200" height="10" /> {/* Заглушка для описания */}
+      <rect x="140" y="70" rx="3" ry="3" width="150" height="10" /> {/* Заглушка для телефона */}
+      <rect x="140" y="90" rx="3" ry="3" width="200" height="10" /> {/* Заглушка для адреса */}
+      <rect x="140" y="110" rx="3" ry="3" width="100" height="10" /> {/* Заглушка для сайта */}
+      <rect x="10" y="150" rx="3" ry="3" width="30" height="30" /> {/* Заглушка для кнопки сохранения */}
+      <rect x="50" y="150" rx="3" ry="3" width="30" height="30" /> {/* Заглушка для кнопки маршрута */}
+    </ContentLoader>
+  );
+};
+
+export default ChosenObjectSkeleton;
---
+++ src/components/SaveMenu/SavedObjects/index.tsx
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import { getObjectById } from '../../../api/overpassApi';
+
+import './styles.css';
+import SavedObjectSkeleton from './Skeleton';
+import SavedObject from './SavedObject';
+
+interface IProps {
+    savedObjectsId: number[]
+    handleDeleteObjectId: (id: number) => void
+}
+
+const SavedObjects = (props: IProps) => {
+    const [savedObjects, setSavedObjects] = useState<JSX.Element[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        setIsLoading(true);
+
+        const timer = setTimeout(async () => {
+            async function handleSetSavedObjects() {
+                const geoObjects = await getObjectById(props.savedObjectsId);
+
+                const savedObjects = geoObjects.map((x, index) => (
+                    <SavedObject handleDeleteObject={props.handleDeleteObjectId} savedObject={x} key={index} />
+                ));
+                setSavedObjects(savedObjects);
+            }
+
+            await handleSetSavedObjects();
+
+            setIsLoading(false);
+        }, 0);
+
+        return () => clearTimeout(timer);
+
+    }, [props.savedObjectsId]);
+
+    return (
+        <>
+            <h2 className="option-title">Избранное:</h2>
+            <div className='saved-contant'>
+                {isLoading ? (
+                    <>
+                        <SavedObjectSkeleton />,
+                        <SavedObjectSkeleton />,
+                        <SavedObjectSkeleton />,
+                        <SavedObjectSkeleton />
+                    </>
+                ) : (savedObjects)}
+            </div>
+        </>
+    );
+}
+
+export default SavedObjects;
