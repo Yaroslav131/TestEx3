a66cd05cf0c7a6949755d29733e59920251630fd
Yaroslav131 <berserk12186@gmail.com>
2023-06-27 22:00:33 +0300

    add toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { Provider } from 'react-redux';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import store from '../src/store/store';
 import MainPage from './pages/mainPage';
 
 import './globalStyles.css';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <MainPage />
+      <ErrorBoundary>
+        <MainPage />
+        <ToastContainer />
+      </ErrorBoundary>
     </Provider>
   );
 }
