f349bf91da1d94bf58de383fa5f511da0b874159
Yaroslav131 <berserk12186@gmail.com>
2023-06-27 22:47:38 +0300

    fiix bug

--- src/store/slices/savedObjectsIdSlice.ts
+++ src/store/slices/savedObjectsIdSlice.ts
@@ -1,7 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-import type { RootState } from '../store';
-
 interface GeoObjectState {
   value: number[];
 }
--- src/store/slices/tagsSlice.ts
+++ src/store/slices/tagsSlice.ts
@@ -1,5 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import type { RootState } from '../store';
+
 
 interface TagsSliceState {
   value: string[];
